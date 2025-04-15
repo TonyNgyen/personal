656b7790e57ce5f197ede5774ea7bb49142a3c97
Tony Nguyen <tonynguyeenn@gmail.com>
2025-04-15 14:29:23 -0700

    Updating information

--- src/components/sections/about/aboutMe/workHistory.tsx
+++ src/components/sections/about/aboutMe/workHistory.tsx
@@ -7,7 +7,7 @@ function WorkHistory() {
       <h2 className="text-xl font-semibold">Choir Power</h2>
       <h3 className="text-sm dark:text-zinc-400 text-zinc-500">Software Engineering Intern</h3>
       <h4 className="text-sm mb-1">
-        Feb 2024 - <span className="text-green-400">Now</span>
+        Feb 2024 - <span className="text-white">April 2025</span>
       </h4>
       <ul className="list-disc list-outside pl-5">
         <li>
